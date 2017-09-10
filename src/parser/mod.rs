20e7ba9ecc36da297bb5082c31d021bd0433cf2d
Willifme <penguinwithwaffle@gmail.com>
2017-09-10 17:25:33 +0100

    Add simple octal parsing and tests

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,5 @@
-use nom::{hex_digit};
-use std::{str, i32};
+use nom::{ hex_digit, oct_digit };
+use std::{ str, i32 };
 
 // Temporarly use a f64 to represent JS numbers
 type JSNumber = f64;
@@ -12,6 +12,28 @@ fn bytes_to_hex(bytes: &[u8]) -> JSNumber {
     // Rust does not support 0x in hexadecimal, so just skip it
     i32::from_str_radix(&string[2..], 16).unwrap() as JSNumber
 }
+
+fn oct_to_hex(bytes: &[u8]) -> JSNumber {
+    // TODO: Fix this unwrap
+    let string = str::from_utf8(bytes).unwrap();
+
+    // TODO: Fix this unwrap
+    // Rust does not support 0o in hexadecimal, so just skip it
+    i32::from_str_radix(&string[2..], 8).unwrap() as JSNumber
+}
+
+named!(pub oct_digits_literal<&[u8], JSNumber>,
+    map!(
+        recognize!(
+            tuple!(
+                alt!(tag!("0o") | tag!("0O")),
+                oct_digit,
+                opt!(complete!(oct_digit))
+            )
+        ),
+        oct_to_hex
+    )
+);
 
 named!(pub hex_digits_literal<&[u8], JSNumber>,
     map!(
--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -14,7 +14,17 @@ mod parser_tests {
     }
 
     #[test]
-    fn parse_a_multiple_hex_digit() {
+    fn parse_a_multiple_hex_digits() {
         assert_eq!(parser::hex_digits_literal(b"0x1AB"), IResult::Done(&b""[..], 427.0));
+    }
+
+    #[test]
+    fn parse_a_single_oct_digit() {
+        assert_eq!(parser::oct_digits_literal(b"0o7"), IResult::Done(&b""[..], 7.0));
+    }
+
+    #[test]
+    fn parse_a_multiple_oct_digits() {
+        assert_eq!(parser::oct_digits_literal(b"0o67"), IResult::Done(&b""[..], 55.0));
     }
 }
