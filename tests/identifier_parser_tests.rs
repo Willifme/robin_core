f90fb6a177d5bdcf9b33f92eab6912818748b53b
Willifme <penguinwithwaffle@gmail.com>
2017-09-13 19:59:52 +0100

    Fix the identifier test

--- tests/identifier_parser_tests.rs
+++ tests/identifier_parser_tests.rs
@@ -7,12 +7,13 @@ mod parser_tests {
     use nom::IResult;
 
     use robin_core::parser::identifier;
+    use robin_core::ast::Expression;
 
     // I think this test is correct
     #[test]
     fn do_not_parse_a_symbol_then_an_alpha() {
         assert_ne!(identifier::identifier_literal(b"+g"),
-                   IResult::Done(&b""[..], "+g".to_string()));
+                   IResult::Done(&b""[..], Expression::Identifier("+g".to_string())));
     }
 
     #[test]
