52fb2bb1b68ab3db2b2c12145ae775a38b9d3ad5
Willifme <me@williamcollier.me.uk>
2017-09-15 09:24:44 +0100

    Add parse_expression function

--- src/parser/expression.rs
+++ src/parser/expression.rs
@@ -1,8 +1,31 @@
+use nom::IResult;
 use ast::Expression;
+use std::boxed::Box;
 
 // Such imports are used to allow for the expr_literal macro to work
 use parser::number::numeric_literal;
 use parser::identifier::identifier_literal;
+
+pub enum ParseResult<'a> {
+    Done(Box<Expression>),
+    Error(&'a [u8], Box<Expression>),
+}
+
+pub fn parse_expression(bytes: &[u8]) -> ParseResult {
+    match expression_literal(bytes) {
+        IResult::Done(rest, complete) => {
+            if rest.len() > 0 {
+                ParseResult::Error(rest, Box::new(complete))
+
+            } else {
+                ParseResult::Done(Box::new(complete))
+            }
+        },
+
+        // TODO: Better error here
+        _ => panic!("Parsing error here"),
+    }
+}
 
 // Temp do bad stuff
 named!(pub expression_literal<Expression>, 
