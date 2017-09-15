2956f255e9c83f8066101e504f4d7ae33d83f277
Willifme <me@williamcollier.me.uk>
2017-09-15 12:16:01 +0100

    Create an error struct and implement Display

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -7,14 +7,22 @@ pub enum ErrorLevel {
     Error,
 }
 
+pub struct Error(pub (ErrorLevel, &'static str));
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{:?}] {}", (self.0).0, (self.0).1)
+    }
+}
+
 // Use a hashmap which contains an error level and a string
-pub struct ErrorStack(pub Vec<(ErrorLevel, &'static str)>);
+pub struct ErrorStack(pub Vec<Error>);
 
 impl fmt::Display for ErrorStack {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for error in &self.0 {
             // TODO: Remove this unwrap
-            write!(f, "[{:?}] {}\n", error.0, error.1).unwrap();
+            write!(f, "{}", error).unwrap();
         }
 
         write!(f, "")
