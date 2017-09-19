225d898b0984299dfe4fdaec4f81ca71e0646c6f
Willifme <penguinwithwaffle@gmail.com>
2017-09-19 19:34:25 +0100

    Add keyword parsing tests

---
+++ tests/expression_parser_tests.rs
@@ -0,0 +1,23 @@
+extern crate robin_core;
+
+extern crate nom;
+
+#[cfg(test)]
+mod parser_tests {
+    use nom::IResult;
+
+    use robin_core::parser::expression;
+    use robin_core::ast::Expression;
+
+    #[test]
+    fn parse_an_identifier_bool_lookalike_suffix() {
+        assert_eq!(expression::expression_literal(b"truedog"),
+                   IResult::Done(&b""[..], Expression::Identifier("truedog".to_string())));
+    }
+
+    #[test]
+    fn parse_an_identifier_bool_lookalike_prefix() {
+        assert_eq!(expression::expression_literal(b"falsecat"),
+                   IResult::Done(&b""[..], Expression::Identifier("falsecat".to_string())));
+    }
+}
