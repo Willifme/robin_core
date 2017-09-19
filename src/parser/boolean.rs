5b192020b51fb201ff0185b6a9e6d417bfeb3757
Willifme <penguinwithwaffle@gmail.com>
2017-09-19 20:58:18 +0100

    Boolean parser now uses value!

--- src/parser/boolean.rs
+++ src/parser/boolean.rs
@@ -1,20 +1,8 @@
-use std::str;
 use ast::Expression;
 
-fn bytes_to_expression(bytes: &[u8]) -> Expression {
-    // TODO: Remove this unwrap
-    match str::from_utf8(bytes).unwrap() {
-        "true" => Expression::Boolean(true), 
-        "false" => Expression::Boolean(false), 
-
-        // TODO: Add this error the error stack
-        _ => panic!("Something has gone wrong with parsing boolean"),
-    }
-}
-
 named!(pub boolean_literal<Expression>,
-    map!(
-        alt!(tag!("true") | tag!("false")),
-        bytes_to_expression
+    alt!(
+        value!(Expression::Boolean(true), tag!("true")) |
+        value!(Expression::Boolean(false), tag!("false"))
     )
 );
