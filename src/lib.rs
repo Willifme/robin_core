e59ce64872c30c283289db332a9d284626a96eee
Willifme <penguinwithwaffle@gmail.com>
2017-09-30 14:01:56 +0100

    Remove old crates and add inline parser module

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
+extern crate pest;
+
 #[macro_use]
-extern crate nom;
+extern crate pest_derive;
 
 extern crate ansi_term;
 
-pub mod parser;
 pub mod ast;
 pub mod error;
+
+/// For some reason, Pest cannot find the grammar file if listed in parser/mod.rs, so I listed it here
+pub mod parser {
+    const _GRAMMAR: &'static str = include_str!("grammar/grammar.pest");
+
+    #[derive(Parser)]
+    #[grammar = "grammar/grammar.pest"]
+    pub struct ExpressionParser;
+}
