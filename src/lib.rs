dc903dcfe01d4d7e07cdffe988085ecd13551b14
Willifme <penguinwithwaffle@gmail.com>
2017-10-08 16:56:53 +0100

    Move parser module to it's own file

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,4 @@ extern crate ansi_term;
 
 pub mod ast;
 pub mod error;
-
-/// For some reason, Pest cannot find the grammar file if listed in parser/mod.rs, so I listed it here
-pub mod parser {
-    const _GRAMMAR: &'static str = include_str!("grammar/grammar.pest");
-
-    #[derive(Parser)]
-    #[grammar = "grammar/grammar.pest"]
-    pub struct ExpressionParser;
-}
+pub mod parser;
