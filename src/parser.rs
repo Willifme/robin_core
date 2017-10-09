78581e02691706362bad09e1cbda5cf84d519b48
Willifme <penguinwithwaffle@gmail.com>
2017-10-09 16:02:35 +0100

    Parse now returns a result. Remove old parse struct for Result instead

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,7 @@
 /// For some reason, Pest cannot find the grammar file if listed in parser/mod.rs, so I listed it here
 use pest::Parser;
+use pest::iterators::Pair;
+use pest::inputs::StrInput;
 
 use ast::Expression;
 
@@ -11,46 +13,52 @@ const _GRAMMAR: &'static str = include_str!("grammar/grammar.pest");
 #[grammar = "grammar/grammar.pest"]
 pub struct ExpressionParser;
 
-pub fn parse(input: String) -> Expression {
-    // TODO: Remove unwrap
-    let mut pairs = ExpressionParser::parse_str(Rule::expression_literal, &input).unwrap();
-
-    let first = pairs.nth(0).unwrap().into_inner().nth(0).unwrap();
-
-    match first.as_rule() {
+fn parse_expression(input: Pair<Rule, StrInput>) -> Expression {
+    match input.as_rule() {
         Rule::boolean_literal =>
             Expression::Boolean(
-                first.into_span().as_str().parse::<bool>().unwrap()
+                input.into_span().as_str().parse::<bool>().unwrap()
             ),
         
         Rule::identifier_literal =>
-            Expression::Identifier(first.into_span().as_str().to_string()),
+            Expression::Identifier(input.into_span().as_str().to_string()),
 
         // TODO: For now, just conver the value into a normal f64. No formatting!
         Rule::decimal_digits_literal =>
             Expression::Number(
-                first.into_span().as_str().parse::<f64>().unwrap()
+                input.into_span().as_str().parse::<f64>().unwrap()
             ),
 
         Rule::binary_digits_literal =>
             // The parse function does not support binary digits, so do it the hard way
             Expression::Number(
-                f64::from(i32::from_str_radix(&first.into_span().as_str()[2..], 2).unwrap())
+                f64::from(i32::from_str_radix(&input.into_span().as_str()[2..], 2).unwrap())
             ),
 
         Rule::oct_digits_literal =>
             // The parse function does not support octal digits, so do it the hard way
             Expression::Number(
-                f64::from(i32::from_str_radix(&first.into_span().as_str()[2..], 8).unwrap())
+                f64::from(i32::from_str_radix(&input.into_span().as_str()[2..], 8).unwrap())
             ),
 
         Rule::hex_digits_literal =>
             // The parse function does not support hexadecimal digits, so do it the hard way
             Expression::Number(
-                f64::from(i32::from_str_radix(&first.into_span().as_str()[2..], 16).unwrap())
+                f64::from(i32::from_str_radix(&input.into_span().as_str()[2..], 16).unwrap())
             ),
 
         // Temporary
         _ => unreachable!()
+    }
+
+}
+
+pub fn parse(input: String) -> Result<Expression, String> {
+    // TODO: Remove unwrap
+    match ExpressionParser::parse_str(Rule::expression_literal, &input) {
+        Ok(mut pair) =>
+            Ok(parse_expression(pair.nth(0).unwrap().into_inner().nth(0).unwrap())),
+        
+        Err(e) => Err(format!("{}", e))
     }
 }
