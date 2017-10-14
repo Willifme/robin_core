93f04acb8ac87e54e5f12babfaf9c6b8bc3a3c92
Willifme <penguinwithwaffle@gmail.com>
2017-10-14 14:54:07 +0100

    Add new tests for symbol lists

--- tests/list_parser_tests.rs
+++ tests/list_parser_tests.rs
@@ -42,4 +42,30 @@ mod parser_tests {
             ]
         )
     }
+
+    #[test]
+    fn parse_a_list_with_symbols() {
+        parses_to!(
+            parser: ExpressionParser,
+            input: "(+)",
+            rule: Rule::list_literal,
+            tokens: [
+                list_literal(0, 3, [identifier_literal(1, 2)])
+            ]
+        )
+    }
+
+    #[test]
+    fn parse_a_list_with_multiple_symbols() {
+        parses_to!(
+            parser: ExpressionParser,
+            input: "(+ 1 1)",
+            rule: Rule::list_literal,
+            tokens: [
+                list_literal(0, 7, 
+                    [identifier_literal(1, 2), decimal_digits_literal(3, 4), decimal_digits_literal(5, 6)]
+                )
+            ]
+        )
+    }
 }
