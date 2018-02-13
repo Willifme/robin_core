5e3068d69e848aa6896268ee8d312a63337cacb7
Willifme <penguinwithwaffle@gmail.com>
2018-02-13 20:41:00 +0000

    Add basic error message list

--- robin_web/src/main.rs
+++ robin_web/src/main.rs
@@ -5,10 +5,8 @@ extern crate robin_core;
 
 use std::borrow::BorrowMut;
 
-use stdweb::unstable::TryInto;
 use stdweb::web::{IEventTarget, INode, document};
 use stdweb::web::event::ClickEvent;
-use stdweb::web::html_element::TextAreaElement;
 
 use robin_core::compiler::Compiler;
 use robin_core::parser::parse;
@@ -18,51 +16,61 @@ fn main() {
 
     let mut compiler = Compiler::new();
 
-    let input: TextAreaElement = document()
-        .get_element_by_id("lisp-input")
-        .unwrap()
-        .first_child()
-        .unwrap()
-        .try_into()
-        .unwrap();
-
-    let output: TextAreaElement = document()
-        .get_element_by_id("js-output")
-        .unwrap()
-        .first_child()
-        .unwrap()
-        .try_into()
-        .unwrap();
-
     let run = document().get_element_by_id("run").unwrap();
 
-    match parse("(+ 1 1)") {
-        Ok(_) => js! {
-            alert("success");
-        },
-        Err(_) => js! {
-            alert("failure");
-        }
-    };
-
     run.add_event_listener(move |_: ClickEvent| {
-        let result = js! {
+        let lisp_input = js! {
             return lisp_input.getSession().getValue();
         };
 
-        js! {
-            var compiled = @{&compiler
-                             .borrow_mut()
-                             .compile(&parse(result.as_str().unwrap())
-                                      .unwrap())};
+        let compiler_list = document() 
+                .get_element_by_id("compiler-list")
+                .unwrap();
 
-            js_output.setValue(compiled);
+        let error_element = document()
+            .create_element("li")
+            .unwrap();
 
-            console.log(js_output);
+        // Clear error nodes
+        while compiler_list.has_child_nodes() {
+            compiler_list
+                .remove_child(&compiler_list.first_child().unwrap())
+                .unwrap();
+        }
 
-            var newWindow = window.open("", "_blank");
+        let parse_result = parse(&lisp_input.as_str().unwrap());
 
-            newWindow.document.write("<script type='text/javascript'>" + compiled + "</script>");
+        if parse_result.is_err() {
+            error_element
+                .set_text_content(
+                    &format!("{}", parse_result.err().unwrap()));
+
+            compiler_list.append_child(&error_element);
+            
+        } else {
+            let parse_result_unwrapped = parse_result.unwrap();
+
+            let compiled = &compiler
+                .borrow_mut()
+                .compile(&parse_result_unwrapped);
+
+            compiler
+                .errors.0
+                .iter()
+                .for_each(|error| {
+                    error_element
+                        .set_text_content(
+                            &format!("{}", error));
+                    compiler_list.append_child(&error_element);
+                });
+
+            js! {
+                js_output.setValue(@{compiled});
+
+                var newWindow = window.open("", "_blank");
+
+                //newWindow.document.write("<script type='text/javascript'>" + @{compiled} + "</script>");
+            }
         }
     });
 
